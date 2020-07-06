1786007ed2ff6b9373d188529c82af9e15808d80
Celt <you@example.com>
2020-07-06 20:28:30 +0300

    Changed port for platform-complete. Changed folio-okapi-registration docker envs. Added extra envs for mod-authtoken

--- docker/folio-okapi-registration/create-deploy.sh
+++ docker/folio-okapi-registration/create-deploy.sh
@@ -39,6 +39,6 @@ cat > /tmp/tenant-enable.json <<END
   "action" : "enable"
 }
 END
-curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d @/tmp/tenant-enable.json $OKAPI_URL/_/proxy/tenants/$TENANT_ID/modules
+curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d @/tmp/tenant-enable.json $OKAPI_URL/_/proxy/tenants/$TENANT_ID/install?deploy=false\&preRelease=false\&tenantParameters=loadSample%3D$SAMPLE_DATA%2CloadReference%3D$REF_DATA
 
 echo Done!
