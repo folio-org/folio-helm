5f66895f069ef46664248613dccffe3ff2e90e78
Celt <you@example.com>
2020-07-07 18:50:07 +0300

    Fix docker post-job script

--- docker/folio-okapi-registration/create-deploy.sh
+++ docker/folio-okapi-registration/create-deploy.sh
@@ -2,12 +2,12 @@
 
 echo ------------------ Getting descriptor ------------------
 curl -s -S -w'\n' 'http://folio-registry.aws.indexdata.com/_/proxy/modules?filter='$MODULE_NAME'&latest=1&full=true'| jq '.[0]' > /tmp/descriptor.json
-MODULE_NAME_VERSION=$(curl -s -S -w'\n' 'http://folio-registry.aws.indexdata.com/_/proxy/modules?filter='$MODULE_NAME'&latest=1&full=true'| jq '.[0].id');
+MODULE_NAME_VERSION=$(curl -s -S -w'\n' 'http://folio-registry.aws.indexdata.com/_/proxy/modules?filter='$MODULE_NAME'&latest=1&full=true'| jq -r '.[0].id');
 
-echo ------------------ Clearning module ------------------
-curl -sL -w '\n' -D - -X DELETE $OKAPI_URL/_/proxy/tenants/$TENANT_ID/modules/$MODULE_NAME_VERSION
-curl -sL -w '\n' -D - -X DELETE $OKAPI_URL/_/discovery/modules/$MODULE_NAME_VERSION
-curl -sL -w '\n' -D - -X DELETE $OKAPI_URL/_/proxy/modules/$MODULE_NAME_VERSION
+#echo ------------------ Clearning module ------------------
+#curl -sL -w '\n' -D - -X DELETE $OKAPI_URL/_/proxy/tenants/$TENANT_ID/modules/$MODULE_NAME_VERSION
+#curl -sL -w '\n' -D - -X DELETE $OKAPI_URL/_/discovery/modules/$MODULE_NAME_VERSION
+#curl -sL -w '\n' -D - -X DELETE $OKAPI_URL/_/proxy/modules/$MODULE_NAME_VERSION
 
 echo ------------------ Pushing module descriptor ------------------
 curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d @/tmp/descriptor.json $OKAPI_URL/_/proxy/modules
@@ -15,8 +15,8 @@ curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d @/tmp/descr
 echo ------------------ Pushing module deployment ------------------
 cat > /tmp/deployment.json <<END
 {
-  "srvcId": $MODULE_NAME_VERSION,
-  "instId": $MODULE_NAME_VERSION,
+  "srvcId": "$MODULE_NAME_VERSION",
+  "instId": "$MODULE_NAME_VERSION",
   "url": "http://$MODULE_NAME"
 }
 END
@@ -35,10 +35,10 @@ curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d @/tmp/tenan
 echo ------------------ Enabling module for tenant ------------------
 cat > /tmp/tenant-enable.json <<END
 {
-  "id": $MODULE_NAME_VERSION,
+  "id": "$MODULE_NAME_VERSION",
   "action" : "enable"
 }
 END
-curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d @/tmp/tenant-enable.json $OKAPI_URL/_/proxy/tenants/$TENANT_ID/install?deploy=false\&preRelease=false\&tenantParameters=loadSample%3D$SAMPLE_DATA%2CloadReference%3D$REF_DATA
+curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d @/tmp/tenant-enable.json $OKAPI_URL/_/proxy/tenants/$TENANT_ID/modules?deploy=false\&preRelease=false\&tenantParameters=loadSample%3D$SAMPLE_DATA%2CloadReference%3D$REF_DATA
 
 echo Done!
