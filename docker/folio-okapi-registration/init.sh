596d87da78ba2b05163261a1e27e16e68aed227d
Stanislav Miroshnichenko <celt@root.in.ua>
2020-07-10 13:35:49 +0300

    Service dockers updated

--- docker/folio-okapi-registration/init.sh
+++ docker/folio-okapi-registration/init.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-if [ -z "$MODULE_NAME" ]
-then
-  # Register all modules from provided list
+if [ -z "$MODULE_NAME" ]; then
+  # Register all modules from provided Platform-complete list
   cat ./platform-complete.json | while read id ; do
+    export MODULE_NAME=${id}
+    ./create-deploy.sh
+  done
+elif [ "$MODULE_NAME" == "deployStripes" ]; then
+  cat ./stripes.json | while read id ; do
     export MODULE_NAME=${id}
     ./create-deploy.sh
   done
