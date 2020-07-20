211e166d7ea28a6a5ebe4183dfa321fe5c6673fb
Stanislav Miroshnichenko <celt@root.in.ua>
2020-07-20 20:50:22 +0300

    platform-complete added 'enable' value. Service dockerfile changed

--- docker/folio-okapi-registration/create-deploy.sh
+++ docker/folio-okapi-registration/create-deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo ------------------ [$MODULE_NAME] Getting descriptor ------------------
 curl -s -S -w'\n' 'http://folio-registry.aws.indexdata.com/_/proxy/modules?filter='$MODULE_NAME'&latest=1&full=true'| jq '.[0]' > /tmp/descriptor.json
@@ -7,18 +7,18 @@ MODULE_NAME_VERSION=$(curl -s -S -w'\n' 'http://folio-registry.aws.indexdata.com
 echo ------------------ [$MODULE_NAME_VERSION] Pushing module descriptor ------------------
 curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d @/tmp/descriptor.json $OKAPI_URL/_/proxy/modules
 
-echo ------------------ [$MODULE_NAME_VERSION] Pushing module deployment ------------------
-DEPLOYMENT_JSON='{ "srvcId": "$MODULE_NAME_VERSION", "instId": "$MODULE_NAME_VERSION", "url": "http://$MODULE_NAME"}'
+echo ------------------ [$MODULE_NAME_VERSION] Pushing module deployment descriptor ------------------
+DEPLOYMENT_JSON="{\"srvcId\":\"$MODULE_NAME_VERSION\",\"instId\":\"$MODULE_NAME_VERSION\",\"url\":\"http://$MODULE_NAME\"}"
 curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d $DEPLOYMENT_JSON $OKAPI_URL/_/discovery/modules
 
 if [ -n "$TENANT_ID" ]; then
 
   echo ------------------ [$MODULE_NAME_VERSION] Creating tenant ------------------
-  TENANT_JSON='{ "id": "$TENANT_ID", "name" : "$TENANT_ID", "description" : "Default tenant" }'
+  TENANT_JSON="{\"id\":\"$TENANT_ID\",\"name\":\"$TENANT_ID\",\"description\":\"Default_tenant\"}"
   curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d $TENANT_JSON $OKAPI_URL/_/proxy/tenants
 
   echo ------------------ [$MODULE_NAME_VERSION] Enabling module for tenant ------------------
-  TENANT_ENABLE_JSON='[{ "id": "$MODULE_NAME_VERSION", "action" : "enable" }]'
+  TENANT_ENABLE_JSON="[{\"id\":\"$MODULE_NAME_VERSION\",\"action\":\"enable\"}]"
   curl -sL -w '\n' -D - -X POST -H "Content-type: application/json" -d $TENANT_ENABLE_JSON $OKAPI_URL/_/proxy/tenants/$TENANT_ID/install?deploy=false\&preRelease=true\&tenantParameters=loadSample%3D$SAMPLE_DATA%2CloadReference%3D$REF_DATA
 
 fi
--- docker/folio-okapi-registration/init.sh
+++ docker/folio-okapi-registration/init.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 if [ -z "$MODULE_NAME" ]; then
-  # Register all modules from provided Platform-complete list
+  # Register all modules from provided platform-complete.json list
   cat ./platform-complete.json | while read id ; do
     export MODULE_NAME=${id}
     ./create-deploy.sh
   done
 elif [ "$MODULE_NAME" == "platform-complete" ]; then
+  # Register UI modules from provided stripes.json list
   cat ./stripes.json | while read id ; do
     export MODULE_NAME=${id}
     ./create-deploy.sh
