fa5bbf31dfc29d538e86066cd4f864c28f5b6f11
Stanislav Miroshnichenko <celt@root.in.ua>
2020-08-28 11:41:17 +0300

    Postjob enabling param. for modules added

--- package_all.sh
+++ package_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for d in */; do
-  if [ "$d" == "pgadmin4/" ] || [ "$d" == "charts/" ]
+  if [ "$d" == "pgadmin4/" ] || [ "$d" == "charts/" ] || [ "$d" == "terraform/" ]
   then
     continue
   fi
