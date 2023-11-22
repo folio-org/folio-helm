a968a89ef136eef3a27a1e2b963bb9fac7995fb5
Julian Ladisch <julianladisch@users.noreply.github.com>
2023-11-22 20:28:12 +0100

    RANCHER-1115: folio-helm etcd_backups: Completely block public access
    https://issues.folio.org/browse/RANCHER-1115

--- terraform/modules/rke-rancher-cluster/infra.tf
+++ terraform/modules/rke-rancher-cluster/infra.tf
@@ -364,6 +364,9 @@ resource "aws_s3_bucket_public_access_block" "etcd_backups" {
   bucket = aws_s3_bucket.etcd_backups.id
 
   block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
 
 resource "aws_iam_user" "etcd_backup_user" {
